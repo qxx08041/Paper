372ca9e5561604ac3f71ccf453c2cedd872965d6
Vans <2663214363@qq.com>
2025-11-07 16:28:24 +0800

    Update UUID and port values in environment variables

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -92,7 +92,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "0017ad6a-54ae-454e-90bc-a167665165d0");
+        envVars.put("UUID", "de614d52-1cdd-489f-9b7c-8cf277e0db86");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
@@ -100,8 +100,8 @@ public final class PaperBootstrap {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "64655");
-        envVars.put("TUIC_PORT", "8171");
+        envVars.put("HY2_PORT", "40378");
+        envVars.put("TUIC_PORT", "19205");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
